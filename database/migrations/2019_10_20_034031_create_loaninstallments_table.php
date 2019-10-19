076abf17dbfd7e3ac8d49c7344ef4856cbc61ba4
orbachinujbuk <orbachinujbuk@gmail.com>
2019-10-20 03:45:00 +0600

    asdasd

--- database/migrations/2019_10_20_034031_create_loaninstallments_table.php
+++ database/migrations/2019_10_20_034031_create_loaninstallments_table.php
@@ -15,7 +15,18 @@ class CreateLoaninstallmentsTable extends Migration
         Schema::create('loaninstallments', function (Blueprint $table) {
             $table->increments('id');
             $table->date('due_date');
-            $table->date('due_date');
+            $table->integer('installment_no')->unsigned();
+            $table->float('installment_principal');
+            $table->float('installment_interest');
+            $table->float('installment_total');
+
+            $table->float('paid_principal');
+            $table->float('paid_interest');
+            $table->float('paid_total');
+
+            $table->float('outstanding_principal');
+            $table->float('outstanding_interest');
+            $table->float('outstanding_total');
 
             $table->integer('loan_id')->unsigned();
             $table->timestamps();
