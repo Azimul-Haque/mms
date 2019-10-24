e028645a8c4fd5f65d9dd3c5ae5eebadfe30d3f9
orbachinujbuk <orbachinujbuk@gmail.com>
2019-10-24 08:50:23 +0600

    ASD

--- resources/views/dashboard/groups/group_transactions/index.blade.php
+++ resources/views/dashboard/groups/group_transactions/index.blade.php
@@ -48,7 +48,7 @@
     <div class="row">
       <div class="col-md-12">
         <div class="table-responsive">
-          <table class="table table-hover table-condensed table-bordered " id="editable">
+          <table class="table table-hover table-condensed table-bordered table-striped " id="editable">
             <thead>
               <tr>
                 <th>P#</th>
