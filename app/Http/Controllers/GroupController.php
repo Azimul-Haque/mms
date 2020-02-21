5008a642e5377d0270cc635a252fbbe08e49418f
orbachinujbuk <orbachinujbuk@gmail.com>
2020-02-21 23:52:31 +0600

    ASD34

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -80,6 +80,18 @@ class GroupController extends Controller
                                     }]);
                          }])
                          ->get();
+        $memberswithoutloan = Member::where('staff_id', $s_id)
+                                    ->where('group_id', $g_id)
+                                    ->where('status', 1) // status 1 means member is Active
+                                    ->orderBy('passbook', 'asc')
+                                    ->with(['loans' => function ($query) use($loan_type, $transaction_date) {
+                                       $query->where('loanname_id', $loan_type)
+                                             ->where('status', 1) // 1 means active loan
+                                             ->with(['loaninstallments' => function ($query) use($transaction_date) {
+                                                 $query->where('due_date', $transaction_date);
+                                              }]);
+                                    }])
+                                    ->get();
         // dd($members);
         return view('dashboard.groups.group_transactions.index')
                         ->withGroups($groups)
