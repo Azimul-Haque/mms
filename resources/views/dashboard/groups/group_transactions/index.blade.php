3307650d836e2ee6e5e4e367d6b4b1d757054d0b
orbachinujbuk <orbachinujbuk@gmail.com>
2020-02-26 02:56:23 +0600

    ASD33

--- resources/views/dashboard/groups/group_transactions/index.blade.php
+++ resources/views/dashboard/groups/group_transactions/index.blade.php
@@ -121,9 +121,7 @@
                           if(!empty($member->savinginstallments->where('savingname_id', 1)->where('due_date', $transactiondate)->first())) {
                             $generalsavingwd = $member->savinginstallments->where('member_id', $member->id)->where('savingname_id', 1)->where('due_date', $transactiondate)->first()->withdraw;
                           }
-                          if(!empty($member->savings->where('savingname_id', 1)->first()->total_amount)) {
-                            $general_saving_balance = $member->savings->where('savingname_id', 1)->first()->total_amount + $member->savings->where('savingname_id', 1)->first()->interest - $member->savings->where('savingname_id', 1)->first()->withdraw;
-                          }
+                          $general_saving_balance = $member->savings->where('savingname_id', 1)->first()->total_amount + $member->savings->where('savingname_id', 1)->first()->interest - $member->savings->where('savingname_id', 1)->first()->withdraw;
                         @endphp
                         <td id="generalsavingwd{{ $loaninstallment->id }}" onchange="loancalcandpost({{ $member->id }}, {{ $loaninstallment->id }}, '{{ $transactiondate }}', {{ $loaninstallment->installment_no }}, {{ $general_saving_balance }}, 1, 0)" class="for_total_generalsavingwd">{{ $generalsavingwd }}</td>
                         @php
