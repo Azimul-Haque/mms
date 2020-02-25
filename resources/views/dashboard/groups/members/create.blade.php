1aea0f9fa0641d1fa4f66785d77d32652f12c882
orbachinujbuk <orbachinujbuk@gmail.com>
2020-02-26 04:17:19 +0600

    ASD3

--- resources/views/dashboard/groups/members/create.blade.php
+++ resources/views/dashboard/groups/members/create.blade.php
@@ -19,6 +19,152 @@
         <div class="panel-heading">Add Member</div>
         {!! Form::open(['route' => ['dashboard.members.store', $staff->id, $group->id], 'method' => 'POST']) !!}
         <div class="panel-body">
+          <div class="row">
+            <div class="col-md-4">
+              {!! Form::label('passbook', 'Passbook No *') !!}
+              {!! Form::number('passbook', null, array('class' => 'form-control', 'placeholder' => 'Passbook No', 'required' => '')) !!}
+            </div>
+            <div class="col-md-4">
+              {!! Form::label('name', 'Member Name *') !!}
+              {!! Form::text('name', null, array('class' => 'form-control', 'placeholder' => 'Member Name', 'required' => '')) !!}
+            </div>
+            <div class="col-md-4">
+              {!! Form::label('fhusband', 'Father/ Husband Name *') !!}
+              {!! Form::text('fhusband', null, array('class' => 'form-control', 'placeholder' => 'Father/ Husband Name', 'required' => '')) !!}
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-4">
+              {!! Form::label('admission_date', 'Admission Date *') !!}
+              {!! Form::text('admission_date', date('F d, Y'), array('class' => 'form-control', 'placeholder' => 'Admission Date', 'required' => '', 'autocomplete' => 'off', 'readonly' => '')) !!}
+            </div>
+            <div class="col-md-4">
+              {!! Form::label('closing_date', 'Closing Date (Optional)') !!}
+              {!! Form::text('closing_date', null, array('class' => 'form-control', 'placeholder' => 'Closing Date (Optional)', 'autocomplete' => 'off', 'readonly' => '')) !!}
+            </div>
+            <div class="col-md-4">
+              {!! Form::label('ishusband', 'Is Husband *') !!}<br/>
+              <label class="radio-inline">
+                <input type="radio" name="ishusband" id="ishusband" value="1" checked> Yes
+              </label>
+              <label class="radio-inline">
+                <input type="radio" name="ishusband" id="ishusband" value="0"> No
+              </label>
+            </div>
+          </div>
+          <div class="row">
+            {{-- <div class="col-md-4">
+              {!! Form::label('marital_status', 'Marital Status *') !!}
+              <select name="marital_status" class="form-control" required>
+                <option selected="" disabled="">Select Marital Status</option>
+                <option value="0">Unmarried</option>
+                <option value="1">Married</option>
+                <option value="2">Divorced</option>
+              </select>
+            </div> --}}
+            {{-- <div class="col-md-4">
+              {!! Form::label('ethnicity', 'Ethnicity *') !!}
+              <select name="ethnicity" class="form-control" required>
+                <option selected="" disabled="">Select Marital Status</option>
+                <option value="0">Non-tribal</option>
+                <option value="1">Tribal</option>
+              </select>
+            </div> --}}
+          </div>
+          <div class="row">
+            {{-- <div class="col-md-4">
+              {!! Form::label('guardian', 'Guardian *') !!}
+              {!! Form::text('guardian', null, array('class' => 'form-control', 'placeholder' => 'Guardian', 'required' => '', 'autocomplete' => 'off')) !!}
+            </div>
+            <div class="col-md-4">
+              {!! Form::label('guardianrelation', 'Guardian Relation *') !!}
+              {!! Form::text('guardianrelation', null, array('class' => 'form-control', 'placeholder' => 'Guardian Relation', 'required' => '', 'autocomplete' => 'off')) !!}
+            </div> --}}
+            
+          </div>
+          <div class="row">
+        {{--     <div class="col-md-4">
+              {!! Form::label('landlord_name', 'Landlord Name (Optional)') !!}
+              {!! Form::text('landlord_name', null, array('class' => 'form-control', 'placeholder' => 'Landlord Name (Optional)', 'autocomplete' => 'off')) !!}
+            </div> --}}
+            {{-- <div class="col-md-4">
+              {!! Form::label('residence_type', 'Residence Type (Optional)') !!}
+              {!! Form::text('residence_type', null, array('class' => 'form-control', 'placeholder' => 'Residence Type (Optional)', 'autocomplete' => 'off')) !!}
+            </div>
+            <div class="col-md-4">
+              {!! Form::label('education', 'Education *') !!}
+              {!! Form::text('education', null, array('class' => 'form-control', 'placeholder' => 'Education', 'required' => '', 'autocomplete' => 'off')) !!}
+            </div>
+            <div class="col-md-4">
+              {!! Form::label('profession', 'Profession *') !!}
+              {!! Form::text('profession', null, array('class' => 'form-control', 'placeholder' => 'Profession', 'required' => '', 'autocomplete' => 'off')) !!}
+            </div> --}}
+          </div>
+          <div class="row">
+            {{-- <div class="col-md-4">
+              {!! Form::label('nid', 'National ID No *') !!}
+              {!! Form::text('nid', null, array('class' => 'form-control', 'placeholder' => 'National ID No', 'required' => '', 'autocomplete' => 'off', 'onkeypress' => 'if(this.value.length==17) return false;')) !!}
+            </div> --}}
+          </div>
+          <div class="row">
+            <div class="col-md-12"><br/>
+              <big><b><u>Present Address</u></b></big><br/>
+            </div>
+            <div class="col-md-4">
+              {!! Form::label('present_district', 'Present District *') !!}
+              {!! Form::text('present_district', 'Thakurgaon', array('class' => 'form-control', 'placeholder' => 'Present District', 'required' => '')) !!}
+            </div>
+            <div class="col-md-4">
+              {!! Form::label('present_upazilla', 'Present Upazilla *') !!}
+              {!! Form::text('present_upazilla', 'Thakurgaon Sadar', array('class' => 'form-control', 'placeholder' => 'Present Upazilla', 'required' => '')) !!}
+            </div>
+            <div class="col-md-4">
+              {!! Form::label('present_village', 'Present Village *') !!}
+              {!! Form::text('present_village', null, array('class' => 'form-control', 'placeholder' => 'Present Village', 'required' => '')) !!}
+            </div>
+            <div class="col-md-4">
+              {!! Form::label('present_phone', 'Present Phone *') !!}
+              {!! Form::text('present_phone', null, array('class' => 'form-control', 'placeholder' => 'Present Phone', 'required' => '')) !!}
+            </div>
+            <div class="col-md-4">
+              {!! Form::label('passbook_fee', 'PassBook Fee') !!}
+              <div class="input-group">
+                <span class="input-group-addon">৳</span>
+                <input id="passbook_fee" type="text" class="form-control" name="passbook_fee" placeholder="PassBook Fee" value="20">
+              </div>
+            </div>
+            <div class="col-md-4">
+              {!! Form::label('admission_fee', 'Admission Fee') !!}
+              <div class="input-group">
+                <span class="input-group-addon">৳</span>
+                <input id="admission_fee" type="text" class="form-control" name="admission_fee" placeholder="Admission Fee" value="30">
+              </div>
+            </div>
+            <div class="col-md-4">
+              {!! Form::label('shared_deposit', 'Shared Deposit *') !!}
+              <div class="input-group">
+                <span class="input-group-addon">৳</span>
+                <input id="shared_deposit" type="text" class="form-control" name="shared_deposit" placeholder="Shared Deposit" required="">
+              </div>
+            </div>
+            {{-- <div class="col-md-6">
+              <big><b><u>Permanent Address (Optional)</u></b></big><br/>
+              {!! Form::label('permanent_district', 'Permanent District') !!}
+              {!! Form::text('permanent_district', 'Thakurgaon', array('class' => 'form-control', 'placeholder' => 'Permanent District')) !!}
+              {!! Form::label('permanent_upazilla', 'Permanent Upazilla') !!}
+              {!! Form::text('permanent_upazilla', null, array('class' => 'form-control', 'placeholder' => 'Permanent Upazilla')) !!}
+              {!! Form::label('permanent_union', 'Permanent Union') !!}
+              {!! Form::text('permanent_union', null, array('class' => 'form-control', 'placeholder' => 'Permanent Union')) !!}
+              {!! Form::label('permanent_post', 'Permanent Post Office') !!}
+              {!! Form::text('permanent_post', null, array('class' => 'form-control', 'placeholder' => 'Permanent Post Office')) !!}
+              {!! Form::label('permanent_village', 'Permanent Village') !!}
+              {!! Form::text('permanent_village', null, array('class' => 'form-control', 'placeholder' => 'Permanent Village ')) !!}
+              {!! Form::label('permanent_phone', 'Permanent Phone (Optional)') !!}
+              {!! Form::text('permanent_phone', null, array('class' => 'form-control', 'placeholder' => 'Permanent Phone (Optional)' )) !!}<br/>
+            </div> --}}
+          </div>
+
+            
           {{-- <div class="row">
             <div class="col-md-4">
               {!! Form::label('passbook', 'Passbook No *') !!}
