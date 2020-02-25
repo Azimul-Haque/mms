05abddc2f63db33a0cf232b0969524b276aa995b
orbachinujbuk <orbachinujbuk@gmail.com>
2020-02-26 03:33:21 +0600

    ASDSD

--- resources/views/dashboard/groups/group_transactions/index.blade.php
+++ resources/views/dashboard/groups/group_transactions/index.blade.php
@@ -85,7 +85,7 @@
                       {{-- if primary then show savings only --}}
                         @php
                           $generalsaving = 0;
-                          echo $member->savinginstallments;
+                          echo $member->savinginstallments->where('savingname_id', 1);
                           if(!empty($member->savinginstallments->where('savingname_id', 1)->where('due_date', $transactiondate)->first())) {
                             $generalsaving = $member->savinginstallments->where('member_id', $member->id)->where('savingname_id', 1)->where('due_date', $transactiondate)->first()->amount;
                             echo $generalsaving;
