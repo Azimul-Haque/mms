c402485eba8cfc6c85f745ddcb42ef82f5e329eb
orbachinujbuk <orbachinujbuk@gmail.com>
2020-03-02 17:43:47 +0600

    ASD33

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -510,9 +510,10 @@ class DashboardController extends Controller
            $loans = Loan::where('loanname_id', $type)->get();
            foreach ($loans as $loan) {
                if($loan->loaninstallments->where('due_date', date('Y-m-d', strtotime($datetodel)))->count() > 1) {
-                foreach ($loan->loaninstallments->where('due_date', date('Y-m-d', strtotime($datetodel))) as $loaninstallment) {
-                    echo '|' . $loaninstallment->id . '|';
-                }
+                // foreach ($loan->loaninstallments->where('due_date', date('Y-m-d', strtotime($datetodel))) as $loaninstallment) {
+                //     echo '|' . $loaninstallment->id . '|';
+                // }
+                $loan->loaninstallments->where('due_date', date('Y-m-d', strtotime($datetodel)))->count();
                } else {
                 echo 1;
                }
